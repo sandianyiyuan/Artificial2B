810941422a637dd768bf7b529517544d225b6559
sandianyiyuan <sandianyiyuan@live.com>
2021-06-05 17:03:53 +0800

    学习 Controller 绑定方法与返回值间的关系

--- Artificial2B-web/src/main/java/host/sdyy/controller/GreetingController.java
+++ Artificial2B-web/src/main/java/host/sdyy/controller/GreetingController.java
@@ -19,11 +19,21 @@ public class GreetingController {
 	@Autowired
 	private GetUserInfoService getUserInfoService;
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="1") String name, Model model) {
-
+	@GetMapping("/greeting3")
+	public void greeting(@RequestParam(name="name", required=false, defaultValue="1") String name, Model model) {
+		// 方法返回值为void 使用请求路径同名的模板
 		getUserInfoService.getUserInfoById(name, model);
-		//这里返回的数据类型是String，但实际上更多的数据通过本函数中Model model传给了前端。返回值String也会被SpringMVC整合为一个ModelAndView，以供前端使用。（Controller可以返回多种数值，比如void、String、ModelAndView。同学们可以自行搜索学习）
-		return "greeting";
+		//这里返回的数据类型是String，但实际上更多的数据通过本函数中Model model传给了前端。返回值String也会被SpringMVC整合为一个ModelAndView，以供前端使用。
+		//（Controller可以返回多种数值，比如void、String、ModelAndView。同学们可以自行搜索学习）
+		//return "userform";		//这里指定返回的模板文件，如果没写则默认为请求路径同名的模板
+	}
+
+	@GetMapping("/greeting4")
+	public String greetinguserform(@RequestParam(name="name", required=false, defaultValue="1") String name, Model model) {
+		// 请求路径与返回String不同，使用retun String的模板
+		getUserInfoService.getUserInfoById(name, model);
+		// 这里返回的数据类型是String，但实际上更多的数据通过本函数中Model model传给了前端。
+		// 返回值String也会被SpringMVC整合为一个ModelAndView，以供前端使用。
+		return "userform";		//这里指定返回的模板文件，如果没写则默认为请求路径同名的模板
 	}
 }
